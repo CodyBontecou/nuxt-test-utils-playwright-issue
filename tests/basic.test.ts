5d419139822e17bd14df08a23f56bb8a36dfbe94
Cody Bontecou <bontecouc@gmail.com>
2024-05-07 19:46:23 -1000

    moving to beforeEach to replicate issue

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from '@nuxt/test-utils/playwright'
 
-test('test', async ({ page, goto }) => {
+test.beforeEach(async ({ page, goto }) => {
+  page.setViewportSize({ width: 800, height: 900 })
   await goto('/', { waitUntil: 'hydration' })
+})
+
+test('test', async ({ page, goto }) => {
   await expect(page.getByRole('heading')).toHaveText('Welcome to Playwright!')
 })
